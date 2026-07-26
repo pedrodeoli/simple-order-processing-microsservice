637fd695f7e599416d5bf6c68110b6456961b4b9
Pedro Couto <pedrodeoliveira@live.com>
2026-07-26 17:30:18 -0300

    set up scanBasePackage

--- src/main/java/com/service/OrderApplication.java
+++ src/main/java/com/service/OrderApplication.java
@@ -3,7 +3,7 @@ package com.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")
 public class OrderApplication {
 
     public static void main(String args[]) {
